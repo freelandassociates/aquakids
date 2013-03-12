ef2c87fb8e68624f63a311f46e99e3e0de7529ef
Paul Simpson-Parry <paulsimpsonparry@gmail.com>
2013-03-12 14:50:02 -0500

    Initial commit of Aquakids Swim School Management System

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aquakids::Application.config.secret_token = '799fb41b8e3ba221667260867ff0fd859f00cf6227ec19458dea3aa2c5399f839cac4e88c6db4544752c8fc7621f738d0b410be7302271989a4f862a3064c9c5'

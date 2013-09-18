7fc4e41c4e2ac898bab78f11269c01f09daa1375
Paul Simpson-Parry <paulsimpsonparry@gmail.com>
2013-09-18 10:02:51 +0100

    Change Cap strategy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ server "192.241.189.150", :web, :app, :db, primary: true
 set :application, "aquakids"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote_cache
+# set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"

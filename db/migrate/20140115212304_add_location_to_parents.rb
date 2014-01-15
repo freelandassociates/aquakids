2f3c7538770aedf3aa4bfea04fbf006c982e2a7d
Paul Simpson-Parry <paulsimpsonparry@gmail.com>
2014-01-15 22:27:38 +0000

    Add Location Id to Parents

--- db/migrate/20140115212304_add_location_to_parents.rb
+++ db/migrate/20140115212304_add_location_to_parents.rb
@@ -2,8 +2,6 @@ class AddLocationToParents < ActiveRecord::Migration
 
   def change
   	add_column :parents, :location_id, :integer
-  	Parent.reset_column_information
-  	Parent.update_all(location_id: '1')
   end
 
 end
